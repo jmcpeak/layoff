d772bcd332bdd0085fc9b2a03a57276c2d36f7db
jmcpeak <jason.mcpeak@gmail.com>
2025-05-21 10:35:46 -0500

    Stitchboard

--- src/app/button/button.tsx
+++ src/app/button/button.tsx
@@ -1,10 +1,15 @@
-import { Button as MuiButton } from '@mui/material';
+'use client';
 
-export default function Button({
-  backgroundColor = '#fff',
-  label = '',
-  primary = false,
-}) {
+import { Button as MuiButton } from '@mui/material';
+import { useEffect, useState } from 'react';
+
+export default function Button({ backgroundColor = '#fff', primary = false }) {
+  const [label, setLabel] = useState('Hello MUI');
+
+  useEffect(() => {
+    setLabel('Hello MUI2');
+  }, []);
+
   return (
     <MuiButton
       name="bob"
